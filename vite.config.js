3a93e57b92d821697dd0210774ac46051611d062
brian772 <ratatito2@gmail.com>
2026-08-13 19:55:21 +0700

    chore: initialize package.json with dependencies and scripts for Vite and Tailwind CSS

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'css/auth.css',
+                'public/css/auth.css',
                 'resources/js/app.js',
             ],
             refresh: true,
