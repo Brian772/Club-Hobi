960aac1746fa28eacf9f2a2c4091ac02db472108
brian772 <ratatito2@gmail.com>
2026-08-13 20:07:16 +0700

    style: update progress item background color and clean up CSS comments in auth views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
 @section('styles')
-<link rel="stylesheet" href="{{ asset('css/auth.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
 @endsection
 
 @section('content')
